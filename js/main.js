c14dbb27997f6205d6ba84d9387d4e3a7aec9586
hhlitval <litvinhh@gmail.com>
2024-05-25 12:18:10 +0200

    Add sticky header styles + js code

--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ navBtn.onclick = () => {
   }
 };
 
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("#header");
+  const hero = document.querySelector("#home");
+  let triggerHeight = hero.offsetHeight - 170;
+
+  if (window.scrollY > triggerHeight) {
+    header.classList.add("header-sticky");
+  } else {
+    header.classList.remove("header-sticky");
+  }
+});
+
 AOS.init({
   once: true,
 });
