ed74b64bb4279f7bd5461b67d2de8dbf2b75c177
hhlitval <litvinhh@gmail.com>
2024-05-25 15:30:21 +0200

    Add social links list to hero section and gotoTop button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ const nav = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const navBtnImg = document.querySelector("#nav-btn-img");
 
+//Preloader
+function hidePreloader() {
+  const preloader = document.getElementById("preloader");
+  preloader.style.display = "none";
+}
+
+window.addEventListener("load", function () {
+  setTimeout(hidePreloader, 1700);
+});
+
+//Hamburger menu
 navBtn.onclick = () => {
   if (nav.classList.toggle("open")) {
     navBtnImg.src = "img/icons/nav-close.svg";
@@ -10,6 +21,7 @@ navBtn.onclick = () => {
   }
 };
 
+//Sticky header
 window.addEventListener("scroll", function () {
   const header = document.querySelector("#header");
   const hero = document.querySelector("#home");
@@ -17,11 +29,14 @@ window.addEventListener("scroll", function () {
 
   if (window.scrollY > triggerHeight) {
     header.classList.add("header-sticky");
+    goToTop.classList.add("reveal");
   } else {
     header.classList.remove("header-sticky");
+    goToTop.classList.remove("reveal");
   }
 });
 
+//AOS animations settings
 AOS.init({
   once: true,
 });
