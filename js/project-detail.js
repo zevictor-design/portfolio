d988d6636774561d2c6b9f79cd0d6d3f1856d2c8
Victor Araújo <zevictor.design@gmail.com>
2026-09-15 10:51:42 -0300

    Center project description below title

--- js/project-detail.js
+++ js/project-detail.js
@@ -346,9 +346,6 @@ function loadProjectData() {
   // Atualizar título do projeto
   document.getElementById('project-title').textContent = project.title;
 
-  // Atualizar título da descrição
-  document.getElementById('project-description-title').textContent = project.descriptionTitle;
-  
   // Atualizar descrição
   document.getElementById('project-description').textContent = project.description;
   
